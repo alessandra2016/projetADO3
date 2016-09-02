664108d232973b827494d7ce55a82086c7f09b16
alessandra2016 <alehvital@gmail.com>
2016-09-02 01:24:13 -0300

    Alteração 2

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -24,22 +24,22 @@ import java.util.logging.Logger;
  */
 public class Agenda extends ConexaoBD {
 
-  
     private static Scanner entrada = new Scanner(System.in);
+    static String nome, strDataNasc, email, telefone;
 
     public void incluir() {
 
         System.out.print("Digite o nome completo do contato: ");
-        String nome = entrada.nextLine();
+        nome = entrada.nextLine();
 
         System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
-        String strDataNasc = entrada.nextLine();
+        strDataNasc = entrada.nextLine();
 
         System.out.print("Digite o e-mail");
-        String email = entrada.nextLine();
+        email = entrada.nextLine();
 
         System.out.print("Digite o telefone no formato 99 99999-9999");
-        String telefone = entrada.nextLine();
+        telefone = entrada.nextLine();
 
         // 1) Abrir conexao
         PreparedStatement stmt = null;
@@ -92,28 +92,63 @@ public class Agenda extends ConexaoBD {
             }
         }
 
-    //3) Fechar conexao
+        //3) Fechar conexao
     }
-    public void pesquisa(){
-         PreparedStatement stmt = null;
+
+    public void pesquisa() {
+        PreparedStatement stmt = null;
         Connection conn = null;
 
         String sql = "SELECT *FROM TB_CONTATO ";
-        
-                
+
     }
-/* 
-    //public void apagar() {
-        PreparedStatement stmt = null ;
-        Connection conn ;
-        String VL_EMAIL = null;
+
+    public void apagar(int id) {
+        PreparedStatement stmt = null;
+        Connection conn;
+
         try {
-             conn.executeUpdate("DELETE FROM CLIENTE WHERE VL_EMAIL = '" + VL_EMAIL + "';");
+            stmt.executeUpdate("DELETE FROM CLIENTE WHERE ID_CONTATO = '" + id + "';");
         } catch (SQLException ex) {
             System.out.println("Erro ao fechar stmt.");
         }
 
-    }*/
+    }
+
+    public void atualizar(String dados, int n, int id) {
+
+        PreparedStatement stmt = null;
+        Connection conn = null;
+
+        try {
+            if (n == 1) {
+                String query = "UPDATE TB_CONTATO SET NM_CONTATO = '" + nome + "', NM_CONTATO = '" + dados + "' where ID_CONTATO = '" + id + "';";
+
+                System.out.println(" Nome atualizado!");
+                stmt.executeUpdate(query); //com é a mesma variavel que esta guardando a String
+            } else if (n == 2) {
+
+                String query = "UPDATE TB_CONTATO SET DT_NASCIMENTO = '" + strDataNasc + "', DT_NASCIMENTO = '" + dados + "' where ID_CONTATO = '" + id + "';";
+
+                System.out.println(" Data de nascimento Atualizado!");
+                stmt.executeUpdate(query);
+            } else if (n == 3) {
+
+                String query = "UPDATE TB_CONTATO SET VL_TELEFONE = '" + telefone + "', VL_TELEFONE = '" + dados + "' where ID_CONTATO = '" + id + "';";
+
+                System.out.println(" Telefone Atualizado!");
+                stmt.executeUpdate(query);
+            } else if (n == 4) {
+                String query = "UPDATE TB_CONTATO SET VL_EMAIL  = '" + email + "', VL_EMAIL = '" + dados + "' where ID_CONTATO = '" + id + "';";
+
+                System.out.println(" E-mail Atualizado!");
+                stmt.executeUpdate(query);
+            }
+
+        } catch (SQLException e) {
+
+        }
+    }
 
     public static void main(String[] args) {
         Agenda instancia = new Agenda();
@@ -123,6 +158,7 @@ public class Agenda extends ConexaoBD {
             System.out.println("(1) Listar contatos");
             System.out.println("(2) Incluir novo contato");
             System.out.println("(3) Deletar contato");
+            System.out.println("(4) Atualizar contato");
             System.out.println("(9) Sair");
             System.out.print("Opção: ");
 
@@ -136,14 +172,61 @@ public class Agenda extends ConexaoBD {
                     instancia.incluir();
                     break;
                 case 3:
-                    //instancia.apagar();
+                    System.out.println(" Informe o ID do usuario que deseja apagar ");
+                    instancia.pesquisa();
+                    int id = entrada.nextInt();
+                    instancia.apagar(id);
                     break;
+                case 4:
+
+                    System.out.println(" Informe o codigo do usuario deseja editar ");
+                    instancia.pesquisa();
+                    int editarUsuario = entrada.nextInt();
+
+                    System.out.println(" Informe qual dado deseja alterar ");
+                    System.out.println(" 1 - Nome ");
+                    System.out.println(" 2 - Data de nascimento ");
+                    System.out.println(" 3 - E-mail ");
+                    System.out.println(" 4 - Telefone ");
+
+                    int opcaoEditar = entrada.nextInt();
+
+                    switch (opcaoEditar) {
+
+                        case 1:
+                            System.out.println(" Digite o novo nome: ");
+                            String nomeNovo = entrada.nextLine();
+                            instancia.atualizar(nomeNovo, opcaoEditar, editarUsuario);
+
+                            break;
+                        case 2:
+                            System.out.println(" Digite a nova Data de nascimento: ");
+
+                            String strNovaData = entrada.nextLine();
+                            instancia.atualizar(strNovaData, opcaoEditar, editarUsuario);
+                            break;
+                        case 3:
+                            System.out.println(" Digite o novo e-mail: ");
+                            String emailNovo = entrada.nextLine();
+                            instancia.atualizar(emailNovo, opcaoEditar, editarUsuario);
+                            break;
+                        case 4:
+                            System.out.println(" Digite o novo telefone: ");
+                            String telNovo = entrada.nextLine();
+                            instancia.atualizar(telNovo, opcaoEditar, editarUsuario);
+                            break;
+                        default:
+                            System.out.println(" opção invalida!! ");
+
+                    }
+
                 case 9:
                     System.exit(0);
                     break;
                 default:
                     System.out.println("OPÇÃO INVÁLIDA");
             }
+
         } while (true);
 
     }
